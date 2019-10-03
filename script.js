8e982dd3dfd5136ba0a701d4af77bc0a10d33cd7
suspectpz <bkquach96@gmail.com>
2019-10-02 22:17:13 -0700

    fixed printdept func

--- script.js
+++ script.js
@@ -29,33 +29,25 @@ const org2_depts = [
   },
 ]
 
-
-const start_tag = '<ul>'
-const end_tag = '</ul>'
-const start_li = '<li>'
-const end_li = '</li>'
-
-function printDepts(depts) {
-	var output = start_tag
-	for (let a = 0, b = depts.length; a < b; a++) {
-		var objNF = JSON.stringify(depts[a].name);
-  	var objF = objNF.replace(/\"/g, "")
-  	output += start_li + objF;
+function printDepts(depts, parent) {
+	if (depts.length > 0) {
+  	var ul = document.createElement('ul');
+    parent.appendChild(ul);
+  }
   
-  	//for(let c = 0, d = org1_depts.) // stuck here ? how would I address the nested values
-  	//                                // such as org1_depts.name.children[a] for example?
-	}
-
-output += end_li
-output += end_tag
-
-return output;
+  for (var i = 0; i < depts.length; i++) {
+  	var li = document.createElement('li');
+    li.innerHTML = depts[i].name;
+    ul.appendChild(li);
+    
+    if (depts[i].children) {
+    	printDepts(depts[i].children, li);
+    }
+  }
 }
 
-outOrg1 = printDepts(org1_depts);
-outOrg2 = printDepts(org2_depts);
+const org1 = document.getElementById("org1");
+const org2 = document.getElementById("org2");
 
-const outputDiv1 = document.getElementById('org1')
-outputDiv1.innerHTML = outOrg1
-const outputDiv2 = document.getElementById('org2')
-outputDiv2.innerHTML = outOrg2
+printDepts(org1_depts, org1);
+printDepts(org2_depts, org2);
